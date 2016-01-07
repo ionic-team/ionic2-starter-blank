b161dfe11c5848689b967caa3b8102eb666caf7b
mhartington <mikehartington@gmail.com>
2016-01-07 11:32:22 -0500

    config

--- app/app.js
+++ app/app.js
@@ -1,10 +1,14 @@
-import {App, Platform} from 'ionic/ionic';
+import {App, Platform, Config} from 'ionic/ionic';
 import {HomePage} from './pages/home/home';
 
 @App({
   template: `
     <ion-nav [root]="root"></ion-nav>
   `,
+  // Check out the config API docs for more info
+  // http://ionicframework.com/docs/v2/api/config/Config/
+  config: {}
+
 })
 export class MyApp {
   constructor(platform: Platform) {
