939e3451e59143f378e69aa4c5a2c2d78a39642f
Tim Lancina <tim@ionic.io>
2016-01-20 10:37:15 -0600

    updates

--- app/app.ts
+++ app/app.ts
@@ -1,25 +1,20 @@
-import {App, Platform, Config} from 'ionic-framework/ionic';
+import {App, Platform} from 'ionic-framework/ionic';
 import {HomePage} from './pages/home/home';
 
-@App({
-  template: `
-    <ion-nav [root]="root"></ion-nav>
-  `,
-  // Check out the config API docs for more info
-  // http://ionicframework.com/docs/v2/api/config/Config/
-  config: {}
+// https://angular.io/docs/ts/latest/api/core/Type-interface.html
+import {Type} from 'angular2/core';
 
+
+@App({
+  template: '<ion-nav [root]="root"></ion-nav>',
+  config: {} // http://ionicframework.com/docs/v2/api/config/Config/
 })
 export class MyApp {
-  constructor(platform: Platform) {
-    this.platform = platform;
-    this.initializeApp();
-    this.root = HomePage;
-  }
+  root: Type = HomePage;
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      console.log('Platform ready');
+  constructor(platform: Platform) {
+    platform.ready().then(() => {
+      // Do any necessary cordova or native calls here now that the platform is ready
     });
   }
 }
