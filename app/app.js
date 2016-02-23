314ec2c9dd645d6f34bfeec805ee836a2fc232d1
Tim Lancina <tim@ionic.io>
2016-02-23 15:00:27 -0600

    chore(): remove constructor parameter decorators
    Babel doesn't support parameter decorators, so use class property
    'parameters' to supply parameter metadata so Angular2 can do DI.

--- app/app.js
+++ app/app.js
@@ -1,5 +1,4 @@
 import {App, Platform} from 'ionic-framework/ionic';
-import {Inject} from 'angular2/core';
 import {HomePage} from './pages/home/home';
 
 
@@ -8,7 +7,11 @@ import {HomePage} from './pages/home/home';
   config: {} // http://ionicframework.com/docs/v2/api/config/Config/
 })
 export class MyApp {
-  constructor(@Inject(Platform) platform) {
+  static get parameters() {
+    return [[Platform]];
+  }
+
+  constructor(platform) {
     this.rootPage = HomePage;
 
     platform.ready().then(() => {
