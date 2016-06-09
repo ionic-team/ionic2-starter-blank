03827650d9b56db6dbfa25bfde3ff899edeb7698
Dan Bucholtz <dan.bucholtz@gmail.com>
2016-06-09 11:20:21 -0500

    updated js starter

--- app/app.js
+++ app/app.js
@@ -1,11 +1,11 @@
-import {Component} from "@angular/core";
+import {Component} from '@angular/core';
 import {Platform, ionicBootstrap} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {HomePage} from './pages/home/home';
 
 
 @Component({
-  template: '<ion-nav [root]="rootPage"></ion-nav>'
+  template: `<ion-nav [root]="rootPage"></ion-nav>`
 })
 export class MyApp {
   static get parameters() {
--- app/pages/home/home.js
+++ app/pages/home/home.js
@@ -1,11 +1,21 @@
-import {Component} from "@angular/core";
-
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor() {
-
+  static get parameters() {
+    return [[NavController]];
   }
+
+  constructor(_navController) {
+    this._navController = _navController;
+  }
+
+  /*
+    pushPage(){
+      this._navController.push(SomeImportedPage, { userId: "12345"});
+    }
+  */
 }
