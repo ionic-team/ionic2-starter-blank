82a2b6181e8ea4f4a1ebb57ad88496502899f218
Dan Bucholtz <dan.bucholtz@gmail.com>
2016-06-09 11:25:16 -0500

    updated ts starter

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,10 +1,16 @@
 import {Component} from "@angular/core";
+import {NavController} from 'ionic-angular';
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor() {
-
+  constructor(private _navController: NavController) {
   }
+
+  /*
+    pushPage(){
+      this._navController.push(SomeImportedPage, { userId: "12345"});
+    }
+  */
 }
