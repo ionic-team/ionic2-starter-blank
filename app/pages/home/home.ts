bc727775425a254e45d023e813dd6bfb284dff7c
Justin Willis <jgw9617@gmail.com>
2016-06-10 15:16:36 -0500

    fix(starters): match angular style guide

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,11 @@
-import {Component} from "@angular/core";
-import {NavController} from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from 'ionic-angular';
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor(private _navController: NavController) {
+  constructor(private navController: NavController) {
   }
 
   /*
