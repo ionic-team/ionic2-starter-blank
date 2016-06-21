d35c2b9fa6aae7f97f8db30c8f254fbdfca033ca
Max Lynch <max@drifty.com>
2016-06-21 15:39:32 -0500

    Simpler blank

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,16 +1,11 @@
 import {Component} from "@angular/core";
 import {NavController} from 'ionic-angular';
-import {DetailPage} from '../detail/detail';
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor(private _navController: NavController) {
+  constructor(private navController: NavController) {
 
-  }
-
-  pushPage(buttonColor: string) {
-    this._navController.push(DetailPage, { color: buttonColor });
   }
 }
