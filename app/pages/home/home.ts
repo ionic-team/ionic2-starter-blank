a1250dfee1de8477c9485df80e4908cc0069227c
Marciel Sousa <marcielmj@gmail.com>
2016-08-23 16:55:16 -0300

    match angular 2 style guide

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
