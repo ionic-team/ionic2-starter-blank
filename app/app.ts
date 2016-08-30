4b638f88396a2801705b81f025730deb5ce4874e
Marciel Sousa <marcielmj@gmail.com>
2016-08-30 14:26:15 -0300

    Change platform to public (#16)

--- app/app.ts
+++ app/app.ts
@@ -11,7 +11,7 @@ import { HomePage } from './pages/home/home';
 export class MyApp {
   rootPage: any = HomePage;
 
-  constructor(platform: Platform) {
+  constructor(public platform: Platform) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
