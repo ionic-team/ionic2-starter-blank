a869bbece767c1b65c707917c6e545efdabd99ac
Chris Griffith <chris.griffith@gmail.com>
2017-01-03 09:01:05 -0800

    refactor(app): update typing so that it matches other starters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { HomePage } from '../pages/home/home';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage = HomePage;
+  rootPage: any = HomePage;
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
