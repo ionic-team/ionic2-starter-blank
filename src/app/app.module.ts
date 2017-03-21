6916f50838cdc6b0a9f7a516ae28d42126c12360
Brandy Carney <brandyscarney@gmail.com>
2017-03-21 12:21:22 -0400

    refactor(app): add module for home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+
+import { HomePageModule } from '../pages/home/home.module';
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HomePageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
