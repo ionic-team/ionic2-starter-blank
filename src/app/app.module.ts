8f0b973957ccd7bff02846927cc5351ff0303d69
roblouie <rob.j.louie@gmail.com>
2017-03-27 13:24:26 -0500

    Update the v3 branch to also support Ionic Native 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { MyApp } from './app.component';
 import { HomePageModule } from '../pages/home/home.module';
 
 @NgModule({
@@ -18,6 +20,10 @@ import { HomePageModule } from '../pages/home/home.module';
   entryComponents: [
     MyApp
   ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
 })
 export class AppModule {}
