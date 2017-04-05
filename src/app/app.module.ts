8f312e38172302397455692242e75f6bf8009ff6
Justin Willis <jgw9617@gmail.com>
2017-04-05 10:55:28 -0500

    chore(native): remove duplicate imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,6 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-
 @NgModule({
   declarations: [
     MyApp,
