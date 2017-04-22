fe283c5d2c2f8d70e0ad269c283d9358703bd224
Daniel Sogl <MTechDE@users.noreply.github.com>
2017-04-22 18:30:22 +0200

    Update app.component.ts
    In your side menu template, you are using a separate method to initialize the app. So in this template, it should be the same.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,18 +4,23 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
+  constructor(private platform: Platform, private statusBar: StatusBar, private splashScreen: SplashScreen) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
     });
   }
 }
